40f5bb07d5595540e7097b810170b524cb766973
rostasedlacek <rostislav.sedlacek@gmail.com>
2025-09-23 13:17:19 +0200

    Update araven.js

--- araven.js
+++ araven.js
@@ -1,10 +1,1 @@
-$(function(){
-  var $nav = $("#header .header-bottom").children();
-  var $top = $("#header .header-top");
 
-  // Přesune navigaci do header-top (na 2. místo za logo)
-  $nav.insertAfter($top.find(".logo"));
-
-  // Odstraní prázdný header-bottom
-  $("#header .header-bottom").remove();
-});
