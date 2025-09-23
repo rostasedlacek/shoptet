f2d02aee640874ba6454cddc55abfc65593b5ca2
rostasedlacek <rostislav.sedlacek@gmail.com>
2025-09-23 13:15:14 +0200

    Update araven.js

--- araven.js
+++ araven.js
@@ -1,6 +1,10 @@
-// custom.js – testovací script
-$(function() {
-  console.log("Custom JS funguje!");
+$(function(){
+  var $nav = $("#header .header-bottom").children();
+  var $top = $("#header .header-top");
+
+  // Přesune navigaci do header-top (na 2. místo za logo)
+  $nav.insertAfter($top.find(".logo"));
+
+  // Odstraní prázdný header-bottom
+  $("#header .header-bottom").remove();
 });
-
-
